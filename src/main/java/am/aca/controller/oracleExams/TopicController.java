b838f206baab7f248006be206d86f5d71014b1fd
Arman Arshakyan <ArmanMikayelovich@gmail.com>
2019-08-08 17:32:54 +0400

    creating restfull API.

--- src/main/java/am/aca/controller/oracleExams/TopicController.java
+++ src/main/java/am/aca/controller/oracleExams/TopicController.java
@@ -1,5 +1,11 @@
 package am.aca.controller.oracleExams;
 
+import am.aca.dto.TopicDto;
+
 public interface TopicController {
     public Object getAllTopics();
+
+    public Object postTopic(TopicDto topicDto);
+
+    public Object deleteTopic(Integer topicId);
 }
--- src/main/java/am/aca/controller/oracleExams/TopicControllerSpringMvcImpl.java
+++ src/main/java/am/aca/controller/oracleExams/TopicControllerSpringMvcImpl.java
@@ -1,14 +1,18 @@
 package am.aca.controller.oracleExams;
 
 import am.aca.controller.oracleExams.TopicController;
+import am.aca.dto.TopicDto;
+import am.aca.entity.TopicEntity;
 import am.aca.service.TopicSerice;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.function.Predicate;
+
 @Controller
-@RequestMapping(value = "oracle-tests/topics")
+@RequestMapping(value = "oracle-exams/topics")
 public class TopicControllerSpringMvcImpl implements TopicController {
     private final TopicSerice topicSerice;
 
@@ -22,5 +26,15 @@ public class TopicControllerSpringMvcImpl implements TopicController {
         ModelAndView model = new ModelAndView("topics");
         model.addObject("topicList", topicSerice.findAll());
         return model;
+    }
+
+    @Override
+    @RequestMapping(method = RequestMethod.POST)
+    public ModelAndView postTopic(TopicDto topicDto) {
+        if (topicDto.getTopicName() != null && !topicDto.getTopicName().equals("")) {
+            topicSerice.save(new TopicEntity(topicDto.getTopicName()));
+        }
+
+        return getAllTopics();
     }
 }
